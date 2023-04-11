b4ecd0164811a05c62311364b627bc9579861b9f
Ankit Nayan <ankitnayan83@gmail.com>
2023-04-11 18:54:45 +0530

    commit

--- controller/post.js
+++ controller/post.js
@@ -97,15 +97,20 @@ const getAllPost = async (req, res, next) => {
         const qNew = req.query.new;
         const qVote = req.query.vote;
         const qTag = req.query.tag;
+        const qTitle = req.query.title;
         let posts;
         if (qNew) {
-            posts = await Post.find({}).sort({ createdAt: -1 });
+            posts = await Post.find(qTitle ? { title: qTitle } : {}).sort({
+                createdAt: -1,
+            });
         } else if (qVote) {
-            posts = await Post.find({}).sort({ votes: -1 });
+            posts = await Post.find(qTitle ? { title: qTitle } : {}).sort({
+                votes: -1,
+            });
         } else if (qTag) {
             posts = await Post.find({ tags: { $in: [qTag] } });
         } else {
-            posts = await Post.find({});
+            posts = await Post.find(qTitle ? { title: qTitle } : {});
         }
         res.status(200).json(posts);
     } catch (error) {
